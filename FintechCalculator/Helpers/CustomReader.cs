add807c568a8dee6257a7c21e1d283e86091e6d5
MiroQurcika <miriani01@yahoo.com>
2021-03-14 00:45:33 +0400

    aaaaaaaa

--- FintechCalculator/Helpers/CustomReader.cs
+++ FintechCalculator/Helpers/CustomReader.cs
@@ -7,13 +7,66 @@ namespace FintechCalculator.Helpers
 {
 	public static class CustomReader
 	{
-		public static Tuple<decimal, decimal> GetCoordinates(string coordinates)
+		public static Tuple <double, double> GetCoordinates(string data)
 		{
 
-			//Code Goes Here
+			double coord_x = -1, coord_y = -1;
 
+			string shape_str = "SHAPE_WKT";
+			int shape_ind = 0;
 
-			return new Tuple<decimal, decimal>(1, 2);
+			for (int i = 0; i < data.Length; i++)
+			{
+				char ch = data[i];
+
+				if (ch == '[' && data[i + 1] == ']') goto ret_val;
+
+				if (ch == shape_str[shape_ind])
+				{
+					shape_ind++;
+
+					if (shape_ind == shape_str.Length)
+					{
+						i += 15;
+
+						string str_x = "";
+						string str_y = "";
+
+						bool is_x = true;
+
+						while (true)
+						{
+							if (data[i] == ',')
+							{
+								coord_y = Convert.ToDouble(str_y);
+
+								break;
+							}
+							else if (data[i] == ' ')
+							{
+								coord_x = Convert.ToDouble(str_x);
+
+								is_x = false;
+							}
+							else if (is_x)
+							{
+								str_x += data[i];
+							}
+							else
+							{
+								str_y += data[i];
+							}
+
+							i++;
+						}
+
+						goto ret_val;
+					}
+				}
+				else shape_ind = 0;
+			}
+
+			ret_val:  return new Tuple <double, double> (coord_x, coord_y);
 		}
 	}
 }
